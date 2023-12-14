6e59e2563d64038e1984672bc1783e59d194117e
alexrodriguez <alexcarvajal2404@gmail.com>
2023-12-13 23:27:51 -0500

    :zap: initial db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { RecipeModule } from './recipe/recipe.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Recipe } from './recipe/entities/recipe.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'monorail.proxy.rlwy.net',
+      port: 33153,
+      username: 'root',
+      password: 'g-31HgBcbb42EHA51d415f2-GAH4ghHb',
+      database: 'railway',
+      entities: [Recipe],
+      synchronize: true,
+    }),
+    RecipeModule,
+    UserModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
